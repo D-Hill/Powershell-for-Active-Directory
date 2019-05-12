b8e1e0c4af2fc5ce5e02a7b74541e9e8d410e4fc
D-Hill <50548190+D-Hill@users.noreply.github.com>
2019-05-12 22:07:43 +0100

    Add files via upload

---
+++ GroupsAndMembers.ps1
@@ -0,0 +1,36 @@
+﻿# defines search term - use * as a wildcard
+$SearchTerm = "cfg_*"
+
+# creates list of groups meeting description
+$Groups = Get-ADGroup -filter * | where {$_.name -like $SearchTerm}
+
+# Loops through list of groups 
+$Results = foreach ($Group in $Groups) {
+
+   # defines variables     
+   $Name = $Group.name
+   $GroupMembers = Get-ADGroupmember $Group
+   $Count = ($GroupMembers | Measure-Object).count
+
+   # Sets 'name' of group member if group is empty by creating a custom object with a 'name' value of "-"
+    If ($Count -lt 1) { $GroupMembers = [pscustomobject]@{ Name = "-" } }
+  
+       # Gets all group members of listed groups
+       foreach ($GroupMember in $GroupMembers) {
+        
+        # defines variable
+        $GroupMemberName = $GroupMember.name
+        
+        # creates a custom object containing data   
+        [pscustomobject]@{
+
+                GroupName = $Name
+                MemberName = $GroupMemberName
+                NumberOfMembers = $Count  
+
+                }
+            }
+  } 
+
+# outputs the result as a table 
+$Results|format-table
