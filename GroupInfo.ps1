00f2289734abdb1e6f265de6a07ed99d0ac90eb0
D-Hill <50548190+D-Hill@users.noreply.github.com>
2019-05-12 22:20:12 +0100

    Add files via upload

---
+++ GroupInfo.ps1
@@ -0,0 +1,36 @@
+﻿
+# defines search term 
+$SearchTerm = "cfg_proxy_*"
+
+# searched for AD groups meeting search term 
+$Groups = get-adgroup -Filter * | where {$_.name -like $searchTerm}
+
+# Loops through list gathering data
+$Results = foreach ($Group in $Groups) {
+
+   # sets variables
+   $Name = $Group.name
+   $Description = (get-adgroup $Group -Properties description).description
+   $Samaccountname = $Groups.SamAccountName
+   $Count = (Get-ADGroupmember $Group | Measure-Object).count
+
+    # creates a custom object containing the data 
+    [pscustomobject]@{
+
+            Name = $Name
+            Samaccountname = $Samaccountname 
+            Description = $Description
+            NumberOfMembers = $Count
+            
+            }
+   
+
+    } 
+
+# outputs the results 
+$Results
+
+
+
+
+
